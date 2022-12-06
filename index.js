429adc31d74e84458f5e25f7cb862b383242a8f8
Sajid Khan Afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2022-12-06 22:54:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+import inquirer from "inquirer";
+import figlet from "figlet";
+import gradient from "gradient-string";
+function randomNumber() {
+    return Math.floor(Math.random() * 2) + 1;
+}
+let userScore = 0;
+let computerScore = 0;
+async function guessNumber() {
+    let answer = await inquirer.prompt({
+        name: "unumber",
+        type: "number",
+        message: "Guess the number between 1 and 2",
+    });
+    // console.log(answer.unumber);
+    let computerNumber = randomNumber();
+    if (computerNumber === answer.unumber) {
+        console.log(`User Guess is right and You win`);
+        userScore += 10;
+        console.log(`
+    Computer Score =${computerScore}
+    Your Score=${userScore}
+    `);
+    }
+    else {
+        console.log(`User Guess is wrong and You Lose`);
+        computerScore += 10;
+        console.log(`
+    Computer Score =${computerScore}
+    Your Score=${userScore}
+    `);
+    }
+    let again = await inquirer.prompt({
+        name: "question",
+        type: "input",
+        message: "Do you want to play again? y/n",
+    });
+    if (again.question === "y") {
+        guessNumber();
+    }
+    else {
+        if (computerScore > userScore) {
+            console.log("Computer wins");
+            var msg = "computer wins";
+        }
+        else if (computerNumber < userScore) {
+            // console.log("User wins");
+            var msg = "User wins";
+        }
+        else {
+            // console.log("Match is tie");
+            var msg = "Match is tie";
+        }
+        //
+        function winner() {
+            console.clear();
+            // const msg = `
+            // text style`;
+            figlet(msg, (err, data) => {
+                console.log(gradient.pastel.multiline(data));
+            });
+        }
+        winner();
+        //
+    }
+}
+guessNumber();
---
+++ index2.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+//inquirer examples
+let questions = [
+  {
+    type: "input",
+    name: "input_type",
+    message: "What is your name?",
+    default: "Iron Man",
+  },
+  {
+    type: "list",
+    name: "name1",
+    message: "What is your favorite programming language?",
+    choices: ["Java", "Python", "JavaScript"],
+    default: "JavaScript",
+  },
+  {
+    type: "checkbox",
+    name: "checkbox_type",
+    message: "What is your favorite programming language?",
+    choices: ["Java", "Python", "JavaScript"],
+    default: "JavaScript",
+  },
+];
+let answer = await inquirer.prompt(questions);
+// console.log(answer);
